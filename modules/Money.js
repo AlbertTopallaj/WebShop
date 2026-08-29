8fc1cc85ad7386d29f0aecb47da86fc5705df5ef
Professor <altopallaj21@gmail.com>
2026-08-29 22:13:49 +0200

    feat: add private fields to attributes, and get functions for amount and currency and add convert function

--- modules/Money.js
+++ modules/Money.js
@@ -1,6 +1,22 @@
 export default class Money {
+    #amount
+    #currency
+
     constructor(amount, currency){
-        this.amount = amount;
-        this.currency = currency;
+        this.#amount = amount;
+        this.#currency = currency;
+    }
+
+    get amount() {
+        return this.#amount;
+    }
+
+    get currency() {
+        return this.currency;
+    }
+    
+    convert(targetCurrency, rate) {
+        const newAmount = this.#amount * rate;
+        return new Money(newAmount, targetCurrency);
     }
 }
