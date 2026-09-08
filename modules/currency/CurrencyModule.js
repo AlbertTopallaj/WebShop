05cef37446e637235a6ac48a44b2d7fe9af71915
Professor <altopallaj21@gmail.com>
2026-09-08 17:02:23 +0200

    feat: add new inputs and methodsAndInputs

--- modules/currency/CurrencyModule.js
+++ modules/currency/CurrencyModule.js
@@ -1,11 +1,14 @@
 export default class CurrencyModule {
     static descriptor = {
         name: "Currency",
-        inputs: [
-            { label: "Price", type: "number" },
-            { label: "Standardcurrency", type: "select", options: ["SEK", "EURO", "DOLLAR"]},
-            { label: "Targetcurrency", type: "select", options: ["SEK", "EURO", "DOLLAR"]}
-        ],
-        output: "table"
+        methodsAndInputs: [
+            {
+                method: "run",
+                input: [
+                    "currency - what type of currency is it, can be USD, EUR and SEK"
+                ],
+                output: "formaterat pris baserat på ursprungsvalutan"
+            }
+        ]
     };
 }
