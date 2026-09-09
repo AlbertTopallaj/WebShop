9d6a4c5a380b13e88bdae309284e31bf6e62348b
Professor <altopallaj21@gmail.com>
2026-09-09 13:32:01 +0200

    feat: add required true to currency

--- modules/currency/CurrencyModule.js
+++ modules/currency/CurrencyModule.js
@@ -6,10 +6,18 @@ export default class CurrencyModule {
                 method: "run",
                 input: [
                     {
+                        name: "cartItems",
+                        type: "reference",
+                        label: "Array of items in the cart",
+                        required: true
+
+                    },
+                    {
                         name: "currency",
                         label: "Currency",
                         type: "select",
-                        options: ["USD", "EUR", "SEK"], 
+                        options: ["USD", "EUR", "SEK"],
+                        required: true
 
                     }
                 ],
