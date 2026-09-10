2bf59796d4070ca6e63514dd7174ed63080ebdd4
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2026-09-10 11:37:44 +0200

    Can now change stock value in db Note: Does not update page info until page reload

--- components/ProductCard/Product.jsx
+++ components/ProductCard/Product.jsx
@@ -1,8 +1,9 @@
 export default class Product {
-    constructor(id, name, price, img) {
+    constructor(id, name, price, img, stock) {
         this.id = id
         this.name = name
         this.price = price
         this.img = img
+        this.stock = stock
     }
 }
