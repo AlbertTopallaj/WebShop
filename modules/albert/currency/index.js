ee15aa90390d59e1819d9705680bc83e8db0b998
Professor <altopallaj21@gmail.com>
2026-09-11 10:08:50 +0200

    fix: change wrong spelled currency

--- modules/albert/currency/index.js
+++ modules/albert/currency/index.js
@@ -39,7 +39,7 @@ export default class CurrencyModule {
 
     async run(cartItems, currency) {
         if(!cartItems || cartItems.length === 0) throw new EmptyCartError();
-        if(!["USD", "EUR", "SEK"].includes(currecy)) throw new UnknownCurrencyError(currency);
+        if(!["USD", "EUR", "SEK"].includes(currency)) throw new UnknownCurrencyError(currency);
 
         const rates = await this.rateClient.getRates();
 
