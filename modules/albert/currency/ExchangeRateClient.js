f2f16d19792df133aaf24a17e20803d05250911e
Professor <altopallaj21@gmail.com>
2026-09-11 10:03:54 +0200

    feat: add RateFetchError to ExchangeRateClient

--- modules/albert/currency/ExchangeRateClient.js
+++ modules/albert/currency/ExchangeRateClient.js
@@ -9,6 +9,9 @@ export class ExchangeRateClient {
         if(this.cache) return this.cache
 
         const res = await fetch("/api/rates");
+        if(!res.ok) {
+            throw new RateFetchError();
+        }
 
         const data = await res.json();
         this.cache = data.rates;
