893927d30ee11e2dbf5c064c66ab5b7527063e1f
Professor <altopallaj21@gmail.com>
2026-09-11 09:21:21 +0200

    feat: add constructor to module

--- modules/albert/index.js
+++ modules/albert/index.js
@@ -1,4 +1,11 @@
+import { ExchangeRateClient } from "./currency/ExchangeRateClient";
+import { TaxTable } from "./currency/TaxTable";
+
 export default class CurrencyModule {
+    constructor(){
+        this.rateClient = new ExchangeRateClient();
+        this.taxTable = new TaxTable();
+    }
     static descriptor = {
         name: "Currency",
         methodsAndInputs: [
