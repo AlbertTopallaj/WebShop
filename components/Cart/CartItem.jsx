4df6a86b1b1c189b4f48d37ba92b072a767531a0
Professor <altopallaj21@gmail.com>
2026-09-11 17:31:10 +0200

    feat: add currency to cart

--- components/Cart/CartItem.jsx
+++ components/Cart/CartItem.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
+import { useCurrency } from "../../modules/albert/currency/CurrencyContext";
 import "./CartItem.css"
 
 export default function CartItem({item, remove}) {
+    const { currency, convertCart } = useCurrency();
+    const [convertedPrice, setConvertedPrice] = useState(item.product.price);
+
+    useEffect(() => {
+        async function convert() {
+            const result = await convertCart([item.product]);
+            if (result) setConvertedPrice(result[0].price);
+         }
+         convert();
+    }, [currency]);
+
     return (
         <div className="cart-item">
             <span className="cart-item-name">{item.product.name}</span>
-            <span className="cart-item-price">{item.product.price}</span>
+            <span className="cart-item-price">{convertedPrice}</span>
             <span className="cart-item-qty">Quantity: {item.quantity}</span>
 
             <button className="cart-item-remove" onClick={() => remove(item.product)}>
