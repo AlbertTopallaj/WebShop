9365273df4c32770141973588290f9816c6a75f6
Professor <altopallaj21@gmail.com>
2026-09-11 10:34:14 +0200

    feat: add category to Product.jsx constructor

--- components/ProductCard/Product.jsx
+++ components/ProductCard/Product.jsx
@@ -1,9 +1,10 @@
 export default class Product {
-    constructor(id, name, price, img, stock) {
+    constructor(id, name, price, img, stock, category) {
         this.id = id
         this.name = name
         this.price = price
         this.img = img
         this.stock = stock
+        this.category = category
     }
 }
