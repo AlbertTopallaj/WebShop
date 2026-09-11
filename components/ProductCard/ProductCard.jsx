75015e22e8df41220c72672589b0cb6cda25f438
Professor <altopallaj21@gmail.com>
2026-09-11 10:57:27 +0200

    feat: add currency to ProductCard

--- components/ProductCard/ProductCard.jsx
+++ components/ProductCard/ProductCard.jsx
@@ -2,11 +2,22 @@ import "./ProductCard.css"
 import {Link} from "react-router";
 import {getCart} from "../CartContext/CartContext.jsx";
 import {useToast} from "../Toast/Toast.jsx";
+import { useEffect, useState } from "react";
+import { useCurrency } from "../../modules/albert/currency/CurrencyContext.jsx" 
 
 export default function ProductCard({product}) {
 
     const {addToCart} = getCart()
     const {toast} = useToast()
+    const {currency, convertCart} = useCurrency();
+    const [convertedPrice, setConvertedPrice] = useState(product.price);
+
+    useEffect(() => {
+        async function convert() {
+            const result = await convertCart([product]);
+            setConvertedPrice(result[0].price);
+        } convert();
+    }, [currency]);
 
     function add(product) {
         addToCart(product);
@@ -19,7 +30,8 @@ export default function ProductCard({product}) {
                 <Link to={`/product?id=${product.id}`}>
                     <img src={product.img[0]} alt={product.name}/>
                     <div className="product-info">
-                        <h2>{product.name}</h2> <p>{product.price}</p>
+                        <h2>{product.name}</h2>
+                        <p>{convertedPrice}</p>
                     </div>
                 </Link>
                 <button className="buyBtn" onClick={() => {
