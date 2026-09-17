a46b4b064b0ca4677925382d2265c243adc25129
Professor <altopallaj21@gmail.com>
2026-09-17 03:44:39 +0200

    feat: add useEffect method in OrderTotal.jsx

---
+++ pages/UserInfo/OrderTotal.jsx
@@ -0,0 +1,16 @@
+
+    export function OrderTotal({cart}) {
+        const { currency, convertCart } = useCurrency();
+        const [total, setTotal] = useState(null);
+
+        useEffect(() => {
+            async function convert(){
+                const products = cart.map(item => item.product);
+                const result = await convertCart(products);
+                if (result) setTotal(result.total);
+            }
+            convert();
+        }, [currency]);
+        
+        return <strong>{total ?? "..."}</strong>
+    }
