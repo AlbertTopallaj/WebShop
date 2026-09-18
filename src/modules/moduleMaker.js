41a261cf333da51fd43c619ba7aa33a3677c742d
Albert Topallaj <161573241+AlbertTopallaj@users.noreply.github.com>
2026-09-18 12:27:37 +0200

    fix: remove CurrencyModule from modulemaker.js

--- src/modules/moduleMaker.js
+++ src/modules/moduleMaker.js
@@ -1,9 +1,6 @@
-import CurrencyModule from "./albert/currency/index.js";
 import ShippingCalculator from "./ShippingCalculator/index.js";
 
 export default {
   ShippingCalculator: new ShippingCalculator(),
-  ShippingCalculatorDescriptor: ShippingCalculator.descriptor,
-  CurrencyModule: new CurrencyModule(),
-  CurrencyModuleDescriptor: CurrencyModule.descriptor
+  ShippingCalculatorDescriptor: ShippingCalculator.descriptor
 }
