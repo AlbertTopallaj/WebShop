411ee53ff76842f66b2280cc7743bc70761091fa
Professor <altopallaj21@gmail.com>
2026-09-18 08:24:30 +0200

    fix: add CurrencyModule to moduleMaker.js

--- src/modules/moduleMaker.js
+++ src/modules/moduleMaker.js
@@ -1,6 +1,9 @@
+import CurrencyModule from "./albert/currency/index.js";
 import ShippingCalculator from "./ShippingCalculator/index.js";
 
 export default {
   ShippingCalculator: new ShippingCalculator(),
   ShippingCalculatorDescriptor: ShippingCalculator.descriptor,
-};
+  CurrencyModule: new CurrencyModule(),
+  CurrencyModuleDescriptor: CurrencyModule.descriptor,
+}
